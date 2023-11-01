a5fb61854f92cb16de34b2bfd8bf48a2b57a64d6
NUMS-half <1272606165@qq.com>
2023-11-01 21:13:10 +0800

    create repo

--- src/main/java/cn/edu/neu/mgzmsys/common/MyBatisPlusConfig.java
+++ src/main/java/cn/edu/neu/mgzmsys/common/MyBatisPlusConfig.java
@@ -17,6 +17,7 @@ public class MyBatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         //数据库类型是MySql，因此参数填写DbType.MYSQL
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
         return interceptor;
     }
 }
