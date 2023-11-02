eb1c4e345573562b984e642fb3ac4937c3b8da80
NUMS-half <1272606165@qq.com>
2023-11-02 18:35:28 +0800

    update child login service

--- src/main/java/cn/edu/neu/mgzmsys/service/IVolunteerService.java
+++ src/main/java/cn/edu/neu/mgzmsys/service/IVolunteerService.java
@@ -13,4 +13,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IVolunteerService extends IService<Volunteer> {
 
+    /**
+     * 登录验证业务
+     * @return 是否成功
+     */
+    boolean login(String username, String password);
 }
