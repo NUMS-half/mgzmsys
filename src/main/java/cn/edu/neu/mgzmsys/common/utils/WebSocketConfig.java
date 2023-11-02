47752cceb99b04546db3f82dacf1bd62ee85eee9
NUMS-half <1272606165@qq.com>
2023-11-03 01:12:37 +0800

    update websocket

--- src/main/java/cn/edu/neu/mgzmsys/common/utils/WebSocketConfig.java
+++ src/main/java/cn/edu/neu/mgzmsys/common/utils/WebSocketConfig.java
@@ -1,4 +1,17 @@
 package cn.edu.neu.mgzmsys.common.utils;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 开启WebSocket支持
+ */
+@Configuration
 public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
