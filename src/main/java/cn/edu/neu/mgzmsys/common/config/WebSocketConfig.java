a58467430f13b6c7e870989b973baf1d8019b13d
NUMS-half <1272606165@qq.com>
2023-11-04 19:43:33 +0800

    update rabbitmq

--- src/main/java/cn/edu/neu/mgzmsys/common/config/WebSocketConfig.java
+++ src/main/java/cn/edu/neu/mgzmsys/common/config/WebSocketConfig.java
@@ -1,17 +1,23 @@
 package cn.edu.neu.mgzmsys.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-/**
- * 开启WebSocket支持
- */
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // 发送消息到客户端的前缀
+        registry.setApplicationDestinationPrefixes("/app"); // 客户端发送消息的前缀
     }
 }
